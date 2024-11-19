b1e39e57889c6a2b8d5b081dac88952e18c6cd52
Evgeniy Poddubnyi <epodd@mdao.team>
2024-11-19 16:47:06 +0100

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import {resolve} from 'path'
 import react from '@vitejs/plugin-react'
-import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-  plugins: [react(), dts()],
+  plugins: [react()],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/main.ts'),

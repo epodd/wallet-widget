43b35ba052bd98d8816039ed50f8fd12970245ee
Evgeniy Poddubnyi <epodd@mdao.team>
2024-11-19 16:26:53 +0100

    fix

---
+++ dist/wallet-widget.js
@@ -0,0 +1,7 @@
+import { jsx as r, Fragment as e } from "react/jsx-runtime";
+function n() {
+  return /* @__PURE__ */ r(e, { children: /* @__PURE__ */ r("div", { children: "widget" }) });
+}
+export {
+  n as App
+};
